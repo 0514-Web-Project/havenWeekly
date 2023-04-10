6ab7e4efe39a1edfce07e1e2b03a072ab1189981
yerim <pppp5097@naver.com>
2023-04-10 21:32:57 +0900

    - add haven map

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <div className="title">헤이븐 주간퀘 가이드</div>
+      <img src="haven_map.png" alt="map" />
+    </div>
+  );
 }
 
 export default App;
