f4bb5b25b192fcc102489484efb9cf445a2dec80
yerim <pppp5097@naver.com>
2023-05-05 20:21:30 +0900

    - recoil 설치 및 기본 설정

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import "@/styles/globals.css";
 import Head from "next/head";
 import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <title>헤이븐 주간퀘 가이드</title>
-      </Head>
-      <Component {...pageProps} />
+      <RecoilRoot>
+        <Head>
+          <title>헤이븐 주간퀘 가이드</title>
+        </Head>
+        <Component {...pageProps} />
+      </RecoilRoot>
     </>
   );
 }
