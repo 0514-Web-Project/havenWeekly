6d6a805fa342d8582c08e141a721915bb8199c6e
yerim <pppp5097@naver.com>
2023-05-05 20:17:48 +0900

    - Quest Type 추가

--- front/src/data/QuestDummy.ts
+++ front/src/data/QuestDummy.ts
@@ -1,4 +1,9 @@
-export const QuestDummy = [
+export const QuestDummy: QuestType[] = [
   { name: "블랙헤븐 내부 : 합금 제네로이드 B 처치", difficulty: 3 },
   { name: "블랙헤븐 내부 : 고철 제네로이드 A 처치", difficulty: 5 },
 ];
+
+export interface QuestType {
+  name: string;
+  difficulty: number;
+}
