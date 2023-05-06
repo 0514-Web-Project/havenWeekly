440b8659182e81ff5583374e59d89bcdd97545b4
yerim <pppp5097@naver.com>
2023-05-06 17:57:16 +0900

    - Footer 디자인 적용

--- front/src/layout/Footer.tsx
+++ front/src/layout/Footer.tsx
@@ -2,9 +2,29 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div>
-      2023 © havenWeekly All rights reserved. havenWeekly is not associated with
-      NEXON Korea.
+    <div className="mt-[60px]">
+      <div className="flex gap-x-[32px] justify-center">
+        <img
+          className="cursor-pointer"
+          src="/images/icon_mail.svg"
+          alt="mail"
+        />
+        <img
+          className="cursor-pointer"
+          src="/images/icon_talk.svg"
+          alt="talk"
+        />
+        <a
+          href="https://github.com/0514-Web-Project/havenWeekly"
+          target="_blank"
+        >
+          <img src="/images/icon_github.svg" alt="github" />
+        </a>
+      </div>
+      <div className="mt-[14px]">
+        2023 © havenWeekly All rights reserved. havenWeekly is not associated
+        with NEXON Korea.
+      </div>
     </div>
   );
 }
