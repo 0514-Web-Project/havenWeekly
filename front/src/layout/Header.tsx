e40a425f631355fc2fe592cb5def8940d4ab5199
yerim <pppp5097@naver.com>
2023-05-07 21:09:17 +0900

    - Header 에 메뉴 아이콘 추가

--- front/src/layout/Header.tsx
+++ front/src/layout/Header.tsx
@@ -2,8 +2,11 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="bg-dark text-[40px] w-[906px] h-[96px] rounded-[18px] text-center leading-[96px]">
-      헤이븐 주간퀘 가이드
+    <div className="relative bg-dark text-[40px] w-full h-[96px] rounded-[18px] text-center leading-[96px]">
+      <div className="w-full">헤이븐 주간퀘 가이드</div>
+      <div className="cursor-pointer w-[42px] absolute right-[30px] top-[32px]">
+        <img src="/images/icon_menu.svg" alt="menu" />
+      </div>
     </div>
   );
 };
