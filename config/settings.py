d840c90855842fcec2471957d7e6b6e7298cd0d3
HyeonjunOh <znzn2260@naver.com>
2023-05-21 02:33:35 +0900

    cors 오류 해결

--- config/settings.py
+++ config/settings.py
@@ -128,3 +128,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
