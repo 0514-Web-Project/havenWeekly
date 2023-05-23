7161064f61cd5c1bc15eb46a598c50d6b0d874aa
yerim <pppp5097@naver.com>
2023-05-23 22:41:28 +0900

    - 퀘스트 api hook 추가

---
+++ front/src/data/quest/useQuestData.ts
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import axios from "axios";
+import { CallbackType, QuestType } from "@/data/type";
+
+const useQuestData = () => {
+  const parseQuestData = (data: any): QuestType[] => {
+    const result: QuestType[] = [];
+
+    data.map((d: any) => {
+      result.push({ name: d.name ?? "", difficulty: d.difficulty ?? 1 });
+    });
+
+    return result;
+  };
+
+  const getQuestList = (callback: CallbackType) => {
+    axios
+      .get(process.env.NEXT_PUBLIC_API_URL + "haven/quest/")
+      .then((res) => {
+        const { data } = res;
+        const result = parseQuestData(data);
+
+        callback.onSuccess(result);
+      })
+      .catch((err) => {
+        callback.onFail(err);
+      });
+  };
+
+  return { getQuestList };
+};
+
+export default useQuestData;
