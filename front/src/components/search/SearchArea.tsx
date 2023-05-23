4a6bd647dde337acc1525fa678bd47b1c2ef95e3
yerim <pppp5097@naver.com>
2023-05-23 22:42:10 +0900

    - 퀘스트 리스트 api 연동

--- front/src/components/search/SearchArea.tsx
+++ front/src/components/search/SearchArea.tsx
@@ -1,17 +1,30 @@
-import { QuestType } from "@/data/QuestDummy";
-import React, { useState } from "react";
+import useQuestData from "@/data/quest/useQuestData";
+import { QuestType } from "@/data/type";
+import React, { useEffect, useState } from "react";
 import SearchInput from "./SearchInput";
 import SearchList from "./SearchList";
 
 const SearchArea = () => {
-  const [searchKeyword, setSearchKeyword] = useState<string>("");
+  const [questOriginList, setQuestOriginList] = useState<QuestType[]>([]);
   const [searchResultList, setSearchResultList] = useState<QuestType[]>([]);
+  const { getQuestList } = useQuestData();
+
+  useEffect(() => {
+    getQuestList({
+      onSuccess: (data) => {
+        setSearchResultList(data);
+        setQuestOriginList(data);
+      },
+      onFail: (err) => {
+        console.error(err);
+      },
+    });
+  }, []);
 
   return (
     <div className="border border-solid border-[#fff] rounded-[12px] p-[14px_12px] h-[259px] w-[48%] sm:w-full">
       <SearchInput
-        searchKeyword={searchKeyword}
-        setSearchKeyword={setSearchKeyword}
+        questList={questOriginList}
         setSearchResultList={setSearchResultList}
       />
       <SearchList searchResultList={searchResultList} />
